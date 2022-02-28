2521f4ef02032b33ad3b267b47560fb06ce433c3
Sebastian Rincon <Seb4594@gmail.com>
2022-02-27 19:09:50 -0600

    socketServer that runs in the PI

--- RPI_CC/socketServer.py
+++ RPI_CC/socketServer.py
@@ -1,43 +1,84 @@
-import socket 
+import socket
+import RPi.GPIO as io
+import select
+import time
 
-HOST = '10.3.141.105' # Server IP or Hostname
-#HOST = '127.0.0.1' # Server IP or Hostname
-#HOST = 'raspberrypi.local'# Server IP or Hostname
-PORT = 10001 # Pick an open Port (1000+ recommended), must match the client sport
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
 
-#managing error exception
-try:
-    s.bind((HOST, PORT))
-except socket.error:
-    print("Bind failed")
-s.listen(5)
-print('Socket awaiting messages')
-(conn, addr) = s.accept()
-print('Connected')
 
-# awaiting for message
-while True:
-    data = conn.recv(1024)
-    data = data.decode()
-    print('I sent a message back in response to: ' + data)
-    reply = ''
+class MotorControl():
+    def setup(self):
+        self.HOST = '10.3.141.1'
+        self.PORT = 10001
 
-     # process your message
-    if data == 'Hello':
-        reply = 'Hi, back!'
-    elif data == 'This is important':
-        reply = 'OK, I have done the important thing you have asked me!'
+        ACK_TEXT = 'text_received'
 
-        #and so on and on until...
-    elif data == 'quit':
-        conn.send('Terminating')
-        break
-    else:
-        repl = 'Unknown command'
+        io.setmode(io.BOARD)
+        io.setup(12,io.OUT)
+        io.setup(33,io.OUT)
 
-        # Sending reply
-        reply = reply.encode()
-        conn.send(reply)
-conn.close() # Close connections
+        self.pwmController = io.PWM(12,100)
+        self._2pwmController = io.PWM(33,100)
+
+        self.direction = 50
+        self.pwmController.start(self.direction)
+        self._2pwmController.start(self.direction)
+
+        stop = False
+
+    def setVelocity(self,dutycycle):
+        print(dutycycle)
+        self.direction = dutycycle
+        self.pwmController.ChangeDutyCycle(int(self.direction))
+        self._2pwmController.ChangeDutyCycle(int(self.direction))
+
+    def sendTextViaSocket(self, message, sock):
+        # encode the text message
+        encodedMessage = bytes(message, 'utf-8')
+
+        # send the data vi the socket to the server
+        sock.sendall(encodedMessage)
+
+        # receive acknowledgment from the server
+        #encodedAckText = sock.recv(1024)
+        #ackText = encodedAckText.decode('utf-8')
+
+        # log if acknowledgment was successful
+        # end if
+    # end function
+
+
+    def connection(self):
+        # instantiate a socket object
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('socket instantiated')
+
+        # bind the socket
+        sock.bind((self.HOST, self.PORT))
+        print('socket binded')
+
+        # start the socket listening
+        sock.listen()
+        print('socket now listening')
+
+        # accept the socket response from the client, and get the connection object
+        conn, addr = sock.accept()      # Note: execution waits here until the client calls sock.connect()
+        print('socket accepted, got connection object')
+
+        myCounter = 0
+        while True:
+            encodedMessage = conn.recv(1024)
+            print(encodedMessage.decode('utf-8'))
+            self.setVelocity(encodedMessage.decode('utf-8'))
+            self.sendTextViaSocket(self.direction, conn)
+
+        # end while
+    # end function
+
+    
+if __name__ == '__main__':
+    ctr = MotorControl()
+    ctr.setup()
+    ctr.connection()
+    while True:
+        direction = input("Enter Direction: ")
+        ctr.setVelocity(direction)
