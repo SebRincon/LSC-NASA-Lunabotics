b39e6c25821527aa76d93da16eb35faf55d7dcbb
seb4594@gmail.com <seb4594@gmail.com>
2022-03-24 19:40:25 +0000

    Added Movement support for the rover sim

---
+++ ROS/catkin_ws/src/rover_sim/SimMove.py
@@ -0,0 +1,53 @@
+# /usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+import math
+import time
+# from turtlesim.msg import Pose
+
+
+def move(velocity_publisher, speed, distance, is_forward):
+    # declare a Twist message to send velocity commands
+    velocity_message = Twist()
+    # get current location
+    global x, y
+    x0 = x  # save the initial location x-coordinate
+    y0 = y  # save the initial location y-coordinate
+    if (is_forward):
+        velocity_message.linear.x = abs(speed)
+    else:
+        velocity_message. linear.x = -abs(speed)
+    distance_moved = 0.0
+    # we publish the velocity at 10 Hz (10 times a second)
+    loop_rate = rospy.Rate(10)
+    while True:
+        rospy.loginfo("Turtlesim moves forwards")
+        velocity_publisher.publish(velocity_message)
+        loop_rate.sleep()
+        # Pythagoras theorem
+        distance_moved = abs(math.sqrt(((x-x0) ** 2) + ((y-y0) ** 2)))
+        print(distance_moved)
+        if not (distance_moved < distance):
+            rospy.loginfo("reached")
+            break
+    # finally, stop the robot when the distance is moved
+    velocity_message.linear.x = 0
+    velocity_publisher.publish(velocity_message)
+
+
+def poseCallback(pose_message):
+    global x
+    global y, yaw
+    x = pose_message.x
+    y = pose_message.y
+    yaw = pose_message.theta
+
+
+if __name__ == '__main__':
+    rospy.init_node('turtlesim_motion_pose', anonymous=True)
+    # declare velocity publisher
+    cmd_vel_topic = '/cmd_demo'
+    velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
+    #position_topic = "/turtle1/pose"
+    #pose_subscriber = rospy.Subscriber(position_topic, Pose, poseCallback)
+    time.sleep(2)
